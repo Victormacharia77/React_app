d9b1f09e7fadf542f3e248af0d9713e8b21f001b
victor <victormacharia.kinyanjui@gmail.com>
2024-02-16 10:12:44 +0300

    learning the fundamentals of react;components.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+import  Message from './Message';
+
+function App() {
+  return <div><Message/></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message (){
+    //JSX: Javascript XML
+    return <h1>Hello Victor</h1>
+}
+
+export default Message;
